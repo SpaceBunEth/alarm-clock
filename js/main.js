ee6f60bc24f865b4425b8d78b96be89bdef5dc56
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-09-15 20:31:27 +0000

    clock handler functions are done, start function starts and manages the seconds, when a it counts over 59, intervals of 1000 milliseconds, interval is cleared and start function is ran from the beinging to get the most current time. Not 100% accurate but within a couple seconds

--- js/main.js
+++ js/main.js
@@ -1,59 +1,71 @@
 console.log("hello World")
 
 function currentTime() {
-const dateTime = new Date() //set var to new current date object
+    const dateTime = new Date() //set var to new current date object
 
-const nameOfDay = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday', 
-    'Saturday'
-];
+    const nameOfDay = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday', 
+        'Saturday'
+    ];
 
-const nameOfMonth = [
-    'January',
-    'Febuary',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-];
+    const nameOfMonth = [
+        'January',
+        'Febuary',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
 
-const holdTime = { // format dateTime object data to holdTime object
-    currentYear: dateTime.getFullYear(),
-    currentMonth: nameOfMonth[dateTime.getMonth()],
-    currentDayName: nameOfDay[dateTime.getDay()],
-    currentDayNum: dateTime.getDate(),
-    currentHour: convertHour(dateTime.getHours()),
-    currentMinute: dateTime.getMinutes(),
-    currentSecond: dateTime.getSeconds()
-};
-return holdTime
+    const holdTime = { // format dateTime object data to holdTime object
+        currentYear: dateTime.getFullYear(),
+        currentMonth: nameOfMonth[dateTime.getMonth()],
+        currentDayName: nameOfDay[dateTime.getDay()],
+        currentDayNum: dateTime.getDate(),
+        currentHour: convertHour(dateTime.getHours()),
+        currentMinute: dateTime.getMinutes(),
+        currentSecond: dateTime.getSeconds(),
+    };
+    return holdTime;
 }
 
 function convertHour(hour) {
     if (hour > 12) {
         hour = hour - 12;
-        console.log('PM')
-        return hour
+        console.log('PM');
+        return [hour, 'PM'];
     } else {
-        console.log('AM')
-        return hour
+        console.log('AM');
+        return [hour, 'AM'];
     }
 }
 
-const global = currentTime();
-console.log(global)
+function Start() {
+    const global = currentTime();
+    console.log(global);
+    let globalsecond = global.currentSecond
+    const intervalID = setInterval(changeSecond, 1000, globalsecond);
 
+    function changeSecond(second) {
+        globalsecond = globalsecond + 1;
+        console.log(globalsecond);
+        if (globalsecond > 59) {
+            clearInterval(intervalID)
+            Start()
+        }
+    }
 
+}
 
 
